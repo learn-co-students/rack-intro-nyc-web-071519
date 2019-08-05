7b47a882a9c51a4ef0c039ccc55f67b76fdac790
Simon R. Glenn <simonrglenn@gmail.com>
2019-08-04 21:05:37 -0400

    Done.

--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper.rb'
 
 describe "Application" do
-  it "should introduce itself" do
+  it "should introduce itself & be stinky" do
     get "/"
-    expect(last_response.body).to include ("Hello, my name is")
+    expect(last_response.body).to include ("Hello, my name is" && "BUTT")
   end
 end
+
